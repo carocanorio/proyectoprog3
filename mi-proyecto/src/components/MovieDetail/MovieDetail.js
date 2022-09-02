29e3afbfc21ec1eb5c5fa898fe272b9d1f780528
carocanorio <ccanoriohellwing@udesa.edu.ar>
2022-09-02 09:28:57 -0300

    termine los detalles, no me anda el map de géneros y me falta el loader

--- mi-proyecto/src/components/MovieDetail/MovieDetail.js
+++ mi-proyecto/src/components/MovieDetail/MovieDetail.js
@@ -24,26 +24,44 @@ class MovieDetail extends Component{
 
     }
 
-    /*
-Género al que pertenece la película.
-link o botón agregar a “favoritos”.*/
+
 
     render(){
         return(
-            <article className='card-container'>
-                <img src={`https://image.tmdb.org/t/p/w342/${this.state.movieInformation.poster_path}`} alt=''/>
-                <h2>{this.state.movieInformation.title}</h2>
-                <p>Rating: {this.state.movieInformation.popularity}</p>
-                <p>Release date: {this.state.movieInformation.release_date}</p>
-                <p>{this.state.movieInformation.overview}</p>
-                <ul>
-                    {
-                        this.state.movieInformation.genres.map((oneGenre, i ) => <li key={oneGenre + i}>{oneGenre.name}</li>)
-                    }
-                </ul>
-                <p className='favorite'> <Link to=''>Add to favourites<span class="material-symbols-outlined">heart_plus </span></Link></p> 
+            <>
+                <h1 className="main-title">Movie detail</h1>
 
-            </article>
+                <section className='movie-series-detail'>
+                    <article className='photo-container'>
+                        <img src={`https://image.tmdb.org/t/p/w1280/${this.state.movieInformation.poster_path}`} alt={this.state.movieInformation.title}/>
+                    </article>
+                    
+                    <article className='data'>
+                        <h3 className='detail-title'>{this.state.movieInformation.title}</h3>
+
+                        <hr className="line"></hr>
+
+                        <p className='detail-info'>Rating: {this.state.movieInformation.popularity}</p>
+
+                        <hr className="line"></hr>
+
+                        <p className='detail-info'>Release date: {this.state.movieInformation.release_date}</p>
+
+                        <hr className="line"></hr>
+
+                        <p className='detail-info'>{this.state.movieInformation.overview}</p>
+
+                        <hr className="line"></hr> 
+
+                        {
+                            this.state.movieInformation.genres.map((oneGenre, i ) => <li key={oneGenre + i}>{oneGenre.name}</li>)
+                        }
+                        <hr className="line"></hr>
+
+                        <p className='favorite'> <Link to=''>Add to favourites<span class="material-symbols-outlined">heart_plus </span></Link></p> 
+                    </article>
+                </section>
+            </>
         )
     }
 }
--- mi-proyecto/src/components/ShowDetail/ShowDetail.js
+++ mi-proyecto/src/components/ShowDetail/ShowDetail.js
@@ -1,17 +1,69 @@
 import React, { Component } from 'react';
+import './ShowDetailStyle.css'
+import {Link} from 'react-router-dom';
 
 class ShowDetail extends Component{
 
-    constructor(){
+    constructor(props){
         super();
         this.state = {
-            
+            id: Number(props.match.params.id),
+            showInformation: {}
         }
     };
 
+    componentDidMount(){
+        fetch(`https://api.themoviedb.org/3/tv/${this.state.id}?api_key=cbe0b85cca8ab920f1387b58d1b0ce3a`)
+        .then( response => response.json() )
+        .then( data => this.setState({
+            showInformation: data
+            },
+            () => console.log(data)
+            ))
+        .catch( error =>	console.log('El error fue: ' + error))
+
+    }
+
+
+
     render(){
         return(
-            <></>
+            <>
+                <h1 className="main-title">Series detail</h1>
+
+                <section className='movie-series-detail'>
+                    <article className='photo-container'>
+                        <img src={`https://image.tmdb.org/t/p/w1280/${this.state.showInformation.poster_path}`} alt={this.state.showInformation.name}/>
+                    </article>
+                    
+                    <article className='data'>
+                        <h3 className='detail-title'>{this.state.showInformation.name}</h3>
+
+                        <hr className="line"></hr>
+
+                        <p className='detail-info'>Rating: {this.state.showInformation.popularity}</p>
+
+                        <hr className="line"></hr>
+
+                        <p className='detail-info'>Aired from {this.state.showInformation.first_air_date} to {this.state.showInformation.last_air_date}</p>
+
+                        <hr className="line"></hr>
+
+                        <p className='detail-info'>{this.state.showInformation.overview}</p>
+
+                        <hr className="line"></hr> 
+
+                        
+                        {
+                            this.state.showInformation.genres.map((oneGenre, i ) => <li key={oneGenre + i}>{oneGenre.name}</li>)
+                        }
+
+                        <hr className="line"></hr>
+
+                        <p className='favorite'> <Link to=''>Add to favourites<span class="material-symbols-outlined">heart_plus </span></Link></p> 
+                    </article>
+                </section>
+            </>
         )
     }
 }
