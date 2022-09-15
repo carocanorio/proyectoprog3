7a555f9bc396c92da0a4d7dc250bb1bb56f0894f
quillenbucciero <abuccierorodriguez@udesa.edu.ar>
2022-09-15 09:59:49 -0300

    pasé detail a screens

--- mi-proyecto/src/App.js
+++ mi-proyecto/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 //Own components//
-import MovieDetail from './components/MovieDetail/MovieDetail';
+import MovieDetail from './screens/MovieDetail/MovieDetail';
 import ShowDetail from './components/ShowDetail/ShowDetail';
 import Header from './components/Header/Header'
 import Home from './screens/Home/Home'
